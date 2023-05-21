7dd33ffabcead00f27671dfa0a8e833d6cf65a98
Zareel Kalam <zareelbwd@gmail.com>
2023-05-21 19:40:39 +0530

    login functionality, cookie, token

--- projbackend/controllers/authController.js
+++ projbackend/controllers/authController.js
@@ -1,5 +1,12 @@
 import User from "../models/userSchema.js";
-import { hashPassword } from "../utils/authHelpers.js";
+import { comparePassword, hashPassword } from "../utils/authHelpers.js";
+import config from "../config/index.js";
+import JWT from "jsonwebtoken";
+
+export const cookieOptions = {
+  expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+  httpOnly: true,
+};
 
 export const signup = async (req, res) => {
   try {
@@ -38,6 +45,59 @@ export const signup = async (req, res) => {
       success: false,
       message: "Error in signing up",
       error,
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //validation
+    if (!email || !password) {
+      return res.status(404).json({
+        success: false,
+        message: "Invalid username or password",
+        error,
+      });
+    }
+    //check if the user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Please login",
+      });
+    }
+    // if the user exists compare password
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.status(200).json({
+        success: false,
+        message: "Invalid password",
+      });
+    } else {
+      const token = await JWT.sign({ _id: user._id }, config.JWT_SECRET, {
+        expiresIn: config.JWT_EXPIRTY,
+      });
+      res.cookie("token", token, cookieOptions);
+      res.status(200).json({
+        success: true,
+        message: "Logged in successfully",
+        user: {
+          name: user.name,
+          lastName: user.lastName,
+          email: user.email,
+          address: user.address,
+        },
+        token,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      //internal server error
+      success: false,
+      message: "Error in signing in",
     });
   }
 };
--- projbackend/routes/authRoutes.js
+++ projbackend/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { signup, signout } from "../controllers/authController.js";
+
+import { signup, login, signout } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+
+router.post("/login", login);
 
 router.get("/signout", signout);
 
